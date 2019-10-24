7a6c472d8b9370d163f23e71546b01270bfa98aa
4680414@qq.com <4680414@qq.com>
2019-10-24 13:51:32 +0800

    完成订阅功能

---
+++ db/publish_version.js
@@ -0,0 +1,67 @@
+[
+    {
+        "software" : "weixunclient",
+        "title" : "威讯云协议",
+        "description" : "威讯云协议是升腾创新的高性能虚拟桌面云协议，该协议可在确保优化带宽占用的同时，确保桌面连接协议稳定和安全，可以随时随地为用户其提供内容极为丰富的最佳桌面体验。",
+        "image" : "./image/weixunclient.png",
+        "span_url" : "http://192.168.12.127:8080/output/public/stable/",
+        "allow_subscribe" : true,
+        "list" : [
+            { 
+                "name" : "最新测试版",
+                "url" : "http://192.168.12.127:8080/output/bin/6.5.0.47_20190929_1937230/",
+                "beta" : true
+            },
+            { 
+                "name" : "6.5正式版",
+                "url" : "http://192.168.12.127:8080/output/public/stable/v6.5.0.13/",
+                "beta" : false
+            },
+            { 
+                "name" : "6.0正式版",
+                "url" : "http://192.168.12.127:8080/output/public/stable/v6.0.0.2/",
+                "beta" : false
+            }
+        ]
+    },
+    {
+        "software" : "sep",
+        "title" : "智能扩展协议",
+        "description" : "智能扩展协议是升腾创新的协议扩展组件，可以依托于多种云桌面协议，提供外设重定向，视频增强等扩展功能，增强云桌面体验的好帮手。",
+        "image" : "./image/sep.png",
+        "span_url" : "http://192.168.12.127:8080/output/public/SEP/Older%20Releases/",
+        "allow_subscribe" : true,
+        "list" : [
+            { 
+                "name" : "4.75稳定版",
+                "url" : "http://192.168.12.127:8080/output/public/SEP/Latest%20Release/SEPV4.75/",
+                "beta" : false
+            },
+            { 
+                "name" : "4.70稳定版",
+                "url" : "http://192.168.12.127:8080/output/public/SEP/Latest%20Release/SEPV4.70/",
+                "beta" : false
+            }
+        ]
+    },
+    {
+        "software" : "other",
+        "title" : "其他版本",
+        "description" : "",
+        "image" : "./image/weixunclient.png",
+        "span_url" : "",
+        "allow_subscribe" : false,
+        "list" : [
+            { 
+                "name" : "威讯云电脑移动端",
+                "url" : "https://risingwater-studio.com/ipainstall/",
+                "beta" : true
+            },
+            { 
+                "name" : "国产化软件版本",
+                "url" : "http://192.168.12.127:8080/output/public/other/nationalization/",
+                "beta" : false
+            }
+        ]
+    }
+]
