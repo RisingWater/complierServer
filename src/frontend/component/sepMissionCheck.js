db6845bb13a5cf1cff208cf71bf5c53cdc92b854
wangxu <4680414@qq.com>
2020-04-02 18:18:10 +0800

    提交龙芯UOS编译选项

--- src/frontend/component/sepMissionCheck.js
+++ src/frontend/component/sepMissionCheck.js
@@ -432,6 +432,19 @@ export class SEPMissionCheckContent extends React.Component {
                + "\"\r\n"
                );
         }
+		
+		//Ubuntu 18.04 mips64el
+        if((this.props.complier_option.platform_node.param.indexOf("-national -arch mips64el -os ubuntu18.04") != -1) )
+        {
+            return ("#! /bin/bash\r\n"
+               +"export LC_CTYPE=\"zh_CN.UTF-8\"\r\n"
+               + env_set
+               +"docker exec wangxu /bin/bash -c \"cd  " + this.props.complier_option.codepath
+               + " && echo centerm | sudo -S ./chroot_build.sh"
+               + Option
+               + "\"\r\n"
+               );
+        }
 
         return ("#! /bin/bash\r\n"
                +"export LC_CTYPE=\"zh_CN.UTF-8\"\r\n"
